32d251bc150846a20fab9f06c3711f0c308f7a51
bagratzakaryan <bagratzakaryan@yahoo.com>
2021-07-25 23:51:05 +0400

    imporve code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
+
+interface DataSource {
+  Customer: string;
+  Amount: number;
+  Product: string;
+  SaleDate: Date;
+}
 
 @Component({
   selector: 'app-root',
@@ -13,18 +20,13 @@ export class AppComponent {
   title = 'epam-angular-course-app';
   birthday = new Date(1988, 3, 15);
   condition = false;
-  source$: Observable<any>;
+  source$: Observable<DataSource[]>;
 
   constructor(private http: HttpClient) {
-    const param = {
-      params: { startDate: '2020-05-10', endDate: '2020-05-15' },
-    };
-
     this.source$ = this.http
-      .get(
-        'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
-        param
-      )
+      .get('https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes', {
+        params: { startDate: '2020-05-10', endDate: '2020-05-15' },
+      })
       .pipe(
         shareReplay(),
         map((data) => (data as any).value.slice(0, 10))

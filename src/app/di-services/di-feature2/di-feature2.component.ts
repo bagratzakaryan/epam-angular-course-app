c127433ecebe8db380994c6b5f2efe6b3eeb02c8
bagratzakaryan <bagratzakaryan@yahoo.com>
2021-08-03 14:04:05 +0400

    change serv feature components ordering

--- src/app/di-services/di-feature2/di-feature2.component.ts
+++ src/app/di-services/di-feature2/di-feature2.component.ts
@@ -1,15 +1,17 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { APP_CONFIG } from '../configs/app.config';
-import { AppConfig } from '../interface/config-interface';
+import { DataService } from '../services/Data.service';
 
 @Component({
   selector: 'app-di-feature2',
   templateUrl: './di-feature2.component.html',
   styleUrls: ['./di-feature2.component.css'],
+  providers: [DataService],
 })
 export class DiFeature2Component implements OnInit {
-  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {
-    // debugger;
+  constructor(private injector: Injector) {
+    console.log(this.injector.get(DataService));
+    console.log(this.injector.get(APP_CONFIG));
   }
 
   ngOnInit(): void {}
--- src/app/di-services/di-feature3/di-feature3.component.ts
+++ src/app/di-services/di-feature3/di-feature3.component.ts
@@ -1,18 +1,14 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { APP_CONFIG } from '../configs/app.config';
-import { DataService } from '../services/Data.service';
+import { AppConfig } from '../interface/config-interface';
 
 @Component({
   selector: 'app-di-feature3',
   templateUrl: './di-feature3.component.html',
   styleUrls: ['./di-feature3.component.css'],
-  providers: [DataService],
 })
 export class DiFeature3Component implements OnInit {
-  constructor(private injector: Injector) {
-    console.log(this.injector.get(DataService));
-    console.log(this.injector.get(APP_CONFIG));
-  }
+  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {}
 
   ngOnInit(): void {}
 }
